ebed71d5e81e8fe933a3d1987a0f9e176f3ccbec
Aditya Raj <130920748+adityaraj0908@users.noreply.github.com>
2024-07-30 08:27:05 +0530

    Update 4.c

--- 4.c
+++ 4.c
@@ -1,70 +1,91 @@
- 
-#include<stdio.h> 
-  
-int main() 
-{ 
-      int i, limit, total = 0, x, counter = 0, time_quantum; 
-      int wait_time = 0, turnaround_time = 0, arrival_time[10], burst_time[10], temp[10]; 
-      float average_wait_time, average_turnaround_time; 
-      printf("\nEnter Total Number of Processes:"); 
-      scanf("%d", &limit); 
-      x = limit; 
-      for(i = 0; i < limit; i++) 
-      { 
-            printf("\nEnter Details of Process[%d]\n", i + 1); 
-  
-            printf("Arrival Time:"); 
-  
-            scanf("%d", &arrival_time[i]); 
-  
-            printf("Burst Time:"); 
-  
-            scanf("%d", &burst_time[i]); 
-  
-            temp[i] = burst_time[i]; 
-      } 
-  
-      printf("\nEnter Time Quantum:\t"); 
-      scanf("%d", &time_quantum); 
-      printf("\nProcess ID\t\tBurst Time\t Turnaround Time\t Waiting Time\n"); 
-      for(total = 0, i = 0; x != 0;) 
-      { 
-            if(temp[i] <= time_quantum && temp[i] > 0) 
-            { 
-                  total = total + temp[i]; 
-                  temp[i] = 0; 
-                  counter = 1; 
-            } 
-             if(temp[i] > 0) 
-            { 
-                  temp[i] = temp[i] - time_quantum; 
-                  total = total + time_quantum; 
-            } 
-            if(temp[i] == 0 && counter == 1) 
-            { 
-                  x--; 
-printf("\nProcess[%d]\t\t%d\t\t %d\t\t\t %d", i + 1, burst_time[i], total - 
-arrival_time[i], total - arrival_time[i] - burst_time[i]); 
-wait_time = wait_time + total - arrival_time[i] - burst_time[i]; 
-turnaround_time = turnaround_time + total - arrival_time[i]; 
-counter = 0; 
-} 
-if(i == limit - 1) 
-{ 
-i = 0; 
-} 
-else if(arrival_time[i + 1] <= total) 
-{ 
-i++; 
-} 
-else 
-{ 
-i = 0; 
-} 
-} 
-average_wait_time = wait_time * 1.0 / limit; 
-average_turnaround_time = turnaround_time * 1.0 / limit; 
-printf("\nAverage Waiting Time:\t%2f", average_wait_time); 
-printf("\nAvg Turnaround Time:\t%2f\n", average_turnaround_time); 
-return 0; 
-} 
+#include <stdio.h>
+#include <stdlib.h>
+
+int s[100], count, res[100];
+
+void dfs(int n, int a[100][100], int source)
+{
+    int i;
+    s[source] = 1;
+    for (i = 1; i <= n; i++)
+    {
+        if (a[source][i] == 1 && s[i] == 0)
+            dfs(n, a, i);
+    }
+    count = count + 1;
+    res[count] = source;
+}
+
+void topologicalDFS(int n, int a[100][100])
+{
+    int i, j;
+    for (i = 1; i <= n; i++)
+        s[i] = 0;
+    count = 0;
+    for (j = 1; j <= n; j++)
+    {
+        if (s[j] == 0)
+            dfs(n, a, j);
+    }
+    for (i = n; i >= 1; i--)
+    {
+        printf("--> %d ", res[i]);
+    }
+    printf("\n");
+}
+
+void topologicalSourceRemoval(int n, int a[100][100], int sc[100])
+{
+    int i, j;
+    for (i = 1; i <= n; i++)
+    {
+        if (sc[i] == 0)
+        {
+            printf("%d\t", i);
+            sc[i] = -1;
+            for (j = 1; j <= n; j++)
+            {
+                if (a[i][j] == 1)
+                {
+                    sc[j] = sc[j] - 1;
+                }
+            }
+        }
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int a[100][100], n, i, j, sc[100], ch;
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+    printf("Enter adjacency matrix:\n");
+    for (i = 1; i <= n; i++)
+        for (j = 1; j <= n; j++)
+            scanf("%d", &a[i][j]);
+
+    printf("1: Topological order using DFS\n2: Source Removal method:\n");
+    scanf("%d", &ch);
+
+    switch(ch)
+    {
+        case 1:
+            printf("Topological order using DFS:\n");
+            topologicalDFS(n, a);
+            break;
+        case 2:
+            for (i = 1; i <= n; i++)
+                sc[i] = 0;
+            for (i = 1; i <= n; i++)
+                for (j = 1; j <= n; j++)
+                    if (a[i][j] == 1)
+                        sc[j] = sc[j] + 1;
+            printf("Topological order using Source Removal:\n");
+            topologicalSourceRemoval(n, a, sc);
+            break;
+        default:
+            exit(0);
+    }
+    return 0;
+}
